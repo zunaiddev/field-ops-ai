db66713a3c5b761d8f6571aad378f56817880bef
zunaiddev <ja7667924@gmail.com>
2026-09-15 14:46:30 +0530

    configured database and basic project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import {Module} from '@nestjs/common';
-import {AppController} from './app.controller.js';
-import {AppService} from './app.service.js';
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT as string, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
