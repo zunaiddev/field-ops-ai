5f65f6ef92ea285bfc24eea3838ea7eab004c7ac
zunaiddev <ja7667924@gmail.com>
2026-09-15 14:26:50 +0530

    configured .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module.js';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module.js';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 }
+
 await bootstrap();
